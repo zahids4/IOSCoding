95263fcccb3e0f72c0054896629399476529bf86
Cuong Nguyen <3936946+cuongptnk@users.noreply.github.com>
2022-04-28 10:42:16 -0300

    Initial Commit

---
+++ Coding Challenge/Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coding_ChallengeApp.swift
+//  Coding Challenge
+//
+//  Created by Wattpad Cuong on 2022-04-28.
+//
+
+import SwiftUI
+
+@main
+struct Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coding Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coding Challenge
+//
+//  Created by Wattpad Cuong on 2022-04-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
